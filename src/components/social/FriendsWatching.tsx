86c4d644a27d67911b0cbab055cb811fed98147d
myjak <80157121+myjak@users.noreply.github.com>
2025-10-16 15:31:40 +0200

    Fix FriendsWatching crash: filter undefined anime recommendations

--- src/components/social/FriendsWatching.tsx
+++ src/components/social/FriendsWatching.tsx
@@ -133,7 +133,7 @@ export function FriendsWatching() {
 
       {/* Anime Grid */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-        {recommendations.map((rec) => (
+        {recommendations.filter(rec => rec && rec.anime && rec.anime.id).map((rec) => (
           <Link
             key={rec.anime.id}
             href={`/anime/${getAnimeSlug(rec.anime)}`}
